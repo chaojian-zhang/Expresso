6924bc25b59b9647c903dc465899c1bcf9762e95
Charles Zhang <szinubuntu@gmail.com>
2023-02-24 11:45:57 -0500

    [Management] Refactor library

---
+++ Expresso/Components/CellsetHelper.cs
@@ -0,0 +1,116 @@
+﻿/* Author: Charles Zhang 📧Email: charles_zhang@totalimagine.com
+ * Version: v0.1.3
+ * ALL MODIFICATIONS MUST INCREMENT VERSION NUMBER.
+ *
+ * Notice for AdomdClient, generally only Microsoft.AnalysisServices.AdomdClient.NetCore.retail.amd64 v19.46.0 works.
+ * 
+ * # Version Changes
+ * 
+ * - Pre v0.1.0: Draft definition
+ * - v0.1.1 (2023-02-24): Deal with multi-dimensional name columns.
+ * - v0.1.2 (2023-02-24): Change contact email address.
+ * - v0.1.3 (2023-02-24): Migrate changes from Expresso project.
+ */
+
+using Microsoft.AnalysisServices.AdomdClient;
+using System;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace DataHelpers
+{
+    public static class CellsetHelper
+    {
+        #region Transformations
+        public static DataTable CellSetToTableNew(this CellSet cellset)
+        {
+            DataTable table = new DataTable("cellset");
+
+            if (cellset.Axes.Count == 2)
+            {
+                Axis columns = cellset.Axes[0];
+                Axis rows = cellset.Axes[1];
+                if (rows.Set.Tuples.Count == 0)
+                    return null;
+
+                CellCollection valuesCell = cellset.Cells;
+                for (int i = 0; i < rows.Set.Hierarchies.Count; i++)
+                    table.Columns.Add(CaptureMDXName(rows.Set.Hierarchies[i].UniqueName));
+                for (int i = 0; i < columns.Set.Tuples.Count; i++)
+                    table.Columns.Add(CaptureMDXName(columns.Set.Tuples[i].Members[0].Caption));
+
+                int rowDimensionCount = rows.Set.Tuples[0].Members.Count;
+                int rowCellValuesCount = columns.Set.Tuples.Count;
+                int rowElementSize = rowDimensionCount + rowCellValuesCount;
+                for (int row = 0; row < rows.Set.Tuples.Count; row++)
+                {
+                    DataRow tableRow = table.NewRow();
+
+                    for (int i = 0; i < rows.Set.Tuples[row].Members.Count; i++)
+                        tableRow[i] = CaptureMDXName(rows.Set.Tuples[row].Members[i].Caption);
+                    for (int i = 0; i < columns.Set.Tuples.Count; i++)
+                        tableRow[i + rows.Set.Hierarchies.Count] = cellset.Cells[row * rowCellValuesCount + i].Value;
+
+                    table.Rows.Add(tableRow);
+                }
+
+                return table;
+            }
+            else if (cellset.Axes.Count == 1)
+            {
+                Axis columns = cellset.Axes[0];
+                CellCollection valuesCell = cellset.Cells;
+
+                for (int i = 0; i < columns.Set.Tuples.Count; i++)
+                    table.Columns.Add(new System.Data.DataColumn(columns.Set.Tuples[i].Members[0].Caption));
+
+                int valuesIndex = 0;
+                DataRow row = row = table.NewRow();
+                for (int k = 0; k < columns.Set.Tuples.Count; k++)
+                {
+                    row[k] = valuesCell[valuesIndex].Value;
+                    valuesIndex++;
+                }
+                table.Rows.Add(row);
+
+                return table;
+            }
+            else throw new ArgumentException("Unrecognized cellset format.");
+
+            string CaptureMDXName(string original)
+            {
+                return Regex.Replace(original, @".*\[(.*?)\]", "$1");
+            }
+        }
+        public static DataTable CellSetToTable(this CellSet cellset)
+        {
+            DataTable table = new DataTable("cellset");
+
+            Axis columns = cellset.Axes[0];
+            Axis rows = cellset.Axes[1];
+            CellCollection valuesCell = cellset.Cells;
+
+            table.Columns.Add(rows.Set.Hierarchies[0].Caption);
+            for (int i = 0; i < columns.Set.Tuples.Count; i++)
+                table.Columns.Add(new System.Data.DataColumn(columns.Set.Tuples[i].Members[0].Caption));
+            int valuesIndex = 0;
+            DataRow row = null;
+
+            for (int i = 0; i < rows.Set.Tuples.Count; i++)
+            {
+                row = table.NewRow();
+
+                row[0] = rows.Set.Tuples[i].Members[0].Caption;
+                for (int k = 1; k <= columns.Set.Tuples.Count; k++)
+                {
+                    row[k] = valuesCell[valuesIndex].Value;
+                    valuesIndex++;
+                }
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+        #endregion
+    }
+}
--- Expresso/Components/InMemoryDB.cs
+++ Expresso/Components/InMemoryDB.cs
@@ -1,5 +1,5 @@
 ﻿/* Author: Charles Zhang 📧Email: charles_zhang@totalimagine.com
- * Version: v0.2.6
+ * Version: v0.2.7
  * ALL MODIFICATIONS MUST INCREMENT VERSION NUMBER.
  * 
  * SQLite in-memory DB. #C# #SQLite In-Memory DB: A powerful single-file library for Oracle (ODBC), Cube (Adomd) and SQLite based SQL processing of data. There is also a python equivalance which is not available under this site yet.
@@ -48,6 +48,7 @@
  * - v0.2.4: Refactor/Merge from Expresso project; Add utility functions; Extract out ParcelDataGrid dependancy.
  * - v0.2.5 (2023-02-24): Change contact email address.
  * - v0.2.6 (2023-02-24): Migrate changes out to dedicated DataTableHelper utility class.
+ * - v0.2.7 (2023-02-24): Migrate changes out to dedicated CellsetHelper utility class.
  */
 
 using ConsoleTables;
@@ -347,98 +348,6 @@ namespace InMemoryDB
     #region Utility Functions
     public static class DataTableExtensions
     {
-        #region Transformations
-        public static DataTable CellSetToTableNew(this CellSet cellset)
-        {
-            DataTable table = new DataTable("cellset");
-
-            if (cellset.Axes.Count == 2)
-            {
-                Axis columns = cellset.Axes[0];
-                Axis rows = cellset.Axes[1];
-                if (rows.Set.Tuples.Count == 0)
-                    return null;
-
-                CellCollection valuesCell = cellset.Cells;
-                for (int i = 0; i < rows.Set.Hierarchies.Count; i++)
-                    table.Columns.Add(CaptureMDXName(rows.Set.Hierarchies[i].UniqueName));
-                for (int i = 0; i < columns.Set.Tuples.Count; i++)
-                    table.Columns.Add(CaptureMDXName(columns.Set.Tuples[i].Members[0].Caption));
-
-                int rowDimensionCount = rows.Set.Tuples[0].Members.Count;
-                int rowCellValuesCount = columns.Set.Tuples.Count;
-                int rowElementSize = rowDimensionCount + rowCellValuesCount;
-                for (int row = 0; row < rows.Set.Tuples.Count; row++)
-                {
-                    DataRow tableRow = table.NewRow();
-
-                    for (int i = 0; i < rows.Set.Tuples[row].Members.Count; i++)
-                        tableRow[i] = CaptureMDXName(rows.Set.Tuples[row].Members[i].Caption);
-                    for (int i = 0; i < columns.Set.Tuples.Count; i++)
-                        tableRow[i + rows.Set.Hierarchies.Count] = cellset.Cells[row * rowCellValuesCount + i].Value;
-
-                    table.Rows.Add(tableRow);
-                }
-
-                return table;
-            }
-            else if (cellset.Axes.Count == 1)
-            {
-                Axis columns = cellset.Axes[0];
-                CellCollection valuesCell = cellset.Cells;
-
-                for (int i = 0; i < columns.Set.Tuples.Count; i++)
-                    table.Columns.Add(new System.Data.DataColumn(columns.Set.Tuples[i].Members[0].Caption));
-
-                int valuesIndex = 0;
-                DataRow row = row = table.NewRow();
-                for (int k = 0; k < columns.Set.Tuples.Count; k++)
-                {
-                    row[k] = valuesCell[valuesIndex].Value;
-                    valuesIndex++;
-                }
-                table.Rows.Add(row);
-
-                return table;
-            }
-            else throw new ArgumentException("Unrecognized cellset format.");
-
-            string CaptureMDXName(string original)
-            {
-                return Regex.Replace(original, @".*\[(.*?)\]", "$1");
-            }
-        }
-        public static DataTable CellSetToTable(this CellSet cellset)
-        {
-            DataTable table = new DataTable("cellset");
-
-            Axis columns = cellset.Axes[0];
-            Axis rows = cellset.Axes[1];
-            CellCollection valuesCell = cellset.Cells;
-
-            table.Columns.Add(rows.Set.Hierarchies[0].Caption);
-            for (int i = 0; i < columns.Set.Tuples.Count; i++)
-                table.Columns.Add(new System.Data.DataColumn(columns.Set.Tuples[i].Members[0].Caption));
-            int valuesIndex = 0;
-            DataRow row = null;
-
-            for (int i = 0; i < rows.Set.Tuples.Count; i++)
-            {
-                row = table.NewRow();
-
-                row[0] = rows.Set.Tuples[i].Members[0].Caption;
-                for (int k = 1; k <= columns.Set.Tuples.Count; k++)
-                {
-                    row[k] = valuesCell[valuesIndex].Value;
-                    valuesIndex++;
-                }
-                table.Rows.Add(row);
-            }
-
-            return table;
-        }
-        #endregion
-
         #region Queries
         public static DataRow Pick(this DataTable table, Func<DataRow, bool> condition)
         {
